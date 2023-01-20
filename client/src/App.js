d2d45cc035e825ec59c7308fb07e2a32117c8327
Mikey Reppy <mikey@aurantiumllc.com>
2023-01-20 15:19:30 -0500

    react tutorial stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// Inside src/App.js
+
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p className="title">Registration Form</p>
+
+      <form className="App">
+        <input type="text" />
+        <input type="email" />
+        <input type="password" />
+        <input type={"submit"}
+          style={{ backgroundColor: "#a1eafb" }} />
+      </form>
+    </>
   );
 }
 
